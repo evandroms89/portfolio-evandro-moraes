30770a3b2f2d0fa507d2b15668bb1f47fc0be0e9
Bowie <marketing@letscode.com.br>
2023-11-16 08:56:42 -0300

    resolvendo active button mobile

--- src/Pages/Home/Navbars/BottomNavBar.jsx
+++ src/Pages/Home/Navbars/BottomNavBar.jsx
@@ -8,56 +8,90 @@ import{HiMiniComputerDesktop} from "react-icons/hi2"
 import{BiSolidMessageAltDetail} from "react-icons/bi"
 
 function BottomNavBar() {
-    console.log(`scroll é: ${window.scrollY}`)
+    
+    const [isActive, setIsActive] = useState(false);
+    const [buttonActive, setButtonActive] = useState("home");
+
+    const homeTop = 0
+    const skillsTop = 778
+    const aboutTop = 1981
+    const projectsTop = 3184
+    const contactTop = 4548
+
+    const activeColorButton = () => {
+        setIsActive(true);
+    };
+
+    const disableColorButton = () => {
+        setIsActive(false);
+    };
+
+    useEffect(() =>{
+       window.addEventListener("scroll", () => {
+        if(window.scrollY < skillsTop) {
+            setButtonActive("home")
+            activeColorButton()
+        } else if(window.scrollY >= skillsTop && window.scrollY < aboutTop) {
+            setButtonActive("skills")
+            activeColorButton()
+        } else if(window.scrollY >= aboutTop && window.scrollY < projectsTop) {
+            setButtonActive("about")
+            activeColorButton()
+        } else if(window.scrollY >= projectsTop && window.scrollY < contactTop) {
+            setButtonActive("projects")
+            activeColorButton()
+        } else if(window.scrollY >= contactTop) {
+            setButtonActive("contact")
+            activeColorButton()
+        }
+        console.log(window.scrollY)
+       })
+    },[])
+
     return (
         <div className="bottom-nav-bar">
                         <Link
-                        activeClass="navbar--active-content"
-                        spy={true}
                         smooth={true}
+                        offset={0}
                         duration={500}
                         to="heroSection"
-                        className="bottom-navbar--content"
+                        className={`bottom-navbar--content ${buttonActive == "home" ? "active" : ""}`}
                         >
                         <AiFillHome />
                         </Link>
                         <Link
-                        activeClass="navbar--active-content"
-                        spy={true}
                         smooth={true}
+                        offset={0}
                         duration={500}
                         to="mySkills"
-                        className="bottom-navbar--content"
+                        className={`bottom-navbar--content ${isActive && buttonActive == "skills" ? "active" : ""}`}
                         >
                         <GiSkills />
                         </Link>
                         <Link
-                        activeClass="navbar--active-content"
-                        spy={true}
                         smooth={true}
+                        offset={-90}
                         duration={500}
                         to="AboutMe"
-                        className="bottom-navbar--content"
+                        className={`bottom-navbar--content ${isActive && buttonActive == "about" ? "active" : ""}`}
                         >
                         <BsFillPersonFill />
                         </Link>
                         <Link
-                        activeClass="navbar--active-content"
-                        spy={true}
                         smooth={true}
+                        offset={-30}
                         duration={500}
                         to="MyPortfolio"
-                        className="bottom-navbar--content"
+                        className={`bottom-navbar--content ${isActive && buttonActive == "projects" ? "active" : ""}`}
                         >
                         <HiMiniComputerDesktop />
                         </Link>
                         <Link
-                        activeClass="navbar--active-content"
-                        spy={true}
                         smooth={true}
+                        offset={-40}
                         duration={500}
                         to="Contact"
-                        className="bottom-navbar--content"
+                        className={`bottom-navbar--content ${isActive && buttonActive == "contact" ? "active" : ""}`}
                         >
                         <BiSolidMessageAltDetail />
                         </Link>
