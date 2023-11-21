a6c0105c5f4037ce7126b79f6b1554428a6d2406
Evandro Moraes <evandroms89@gmail.com>
2023-11-21 16:37:34 -0300

    arrumando footer

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -5,65 +5,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer--container">
-        <div className="footer--items">
-          <ul>
-            <li>
-              <Link
-                smooth={true}
-                offset={-70}
-                duration={500}
-                to="heroSection"
-                className="footer--nav"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                smooth={true}
-                offset={-70}
-                duration={500}
-                to="mySkills"
-                className="footer--nav"
-              >
-                Habilidades
-              </Link>
-            </li>
-            <li>
-              <Link
-                smooth={true}
-                offset={-70}
-                duration={500}
-                to="AboutMe"
-                className="footer--nav"
-              >
-                Sobre
-              </Link>
-            </li>
-            <li>
-              <Link
-                smooth={true}
-                offset={-70}
-                duration={500}
-                to="MyPortfolio"
-                className="footer--nav"
-              >
-                Projetos
-              </Link>
-            </li>
-            <li>
-              <Link
-                smooth={true}
-                offset={-70}
-                duration={500}
-                to="Contact"
-                className="footer--nav"
-              >
-                Contato
-              </Link>
-            </li>
-          </ul>
-        </div>
+          <p>&copy; Desenvolvido por Evandro Moraes</p>
     </footer>
   );
 }
